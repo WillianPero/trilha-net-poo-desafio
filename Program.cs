55508391f1b0837b09f8d80ea05441ef945b3534
WillianPero <161774552+WillianPero@users.noreply.github.com>
2025-06-08 19:27:29 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
-﻿using DesafioPOO.Models;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main()
+    {
+        // Criando instâncias dos smartphones
+        Smartphone iphone = new Iphone("123456789", "iPhone 13", "IMEI123456", 128);
+        Smartphone nokia = new Nokia("987654321", "Nokia 3310", "IMEI987654", 32);
+
+        // Testando funcionalidades básicas
+        Console.WriteLine("Testando iPhone:");
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+
+        Console.WriteLine("\nTestando Nokia:");
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+    }
+}
